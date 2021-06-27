e91dc27ffb4b28a55ff5ede930155638152e9e84
sswellington <wellws@pm.me>
2021-06-27 13:03:57 -0300

    add: matrix

---
+++ src/python/matrix/soma.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""
+	Soma de matrizes
+	Escreva a função soma_matrizes(m1, m2) que recebe 2 matrizes 
+	e devolve uma matriz que represente sua soma 
+	caso as matrizes tenham dimensões iguais. 
+	Caso contrário, a função deve devolver False.
+	Exemplos:
+	m1 = [[1, 2, 3], [4, 5, 6]]
+	m2 = [[2, 3, 4], [5, 6, 7]]
+	soma_matrizes(m1, m2) => [[3, 5, 7], [9, 11, 13]]
+	m1 = [[1], [2], [3]]
+	m2 = [[2, 3, 4], [5, 6, 7]]
+	soma_matrizes(m1, m2) => False
+"""
+
+def dimensões(m1,m2):
+	line = len(m1)
+	column = len(m1[0])
+
+	line2 = len(m2)
+	column2 = len(m2[0])
+
+	if((line == line2) and (column2 == column)): 
+		return True
+	else:		
+		return False
+
+
+def soma_matrizes(m1,m2):
+
+	if (dimensões(m1,m2)):
+		
+		for i in range(len(m1)):
+			for j in range(len(m1[0])):
+				
+				m1[i][j] = m1[i][j] + m2[i][j]
+
+		return m1
+
+	else: 
+		return False
+
+
+if __name__ == "__main__":
+	m1 = [[1], 
+       	  [2], 
+          [3]]
+ 
+	m2 = [[2, 3, 4], 
+       	  [5, 6, 7]]
+  
+	if((soma_matrizes(m1,m2)) == False):
+		print('False')
+	else:
+		print(soma_matrizes(m1,m2)[:])
+  
