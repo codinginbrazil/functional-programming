7b978f0c9d01a714141a9aa75e570af30de400cf
sswellington <wellws@pm.me>
2021-06-27 13:00:02 -0300

    add: string

---
+++ doc/string.md
@@ -0,0 +1,7 @@
+## String
+
+### Nome na vertical
+Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+
+### Menor palavra
+Programa que lê duas palavras da entrada e, na saída, imprime a menor palavra
---
+++ src/python/string/vertical.py
@@ -0,0 +1,10 @@
+""" Nome na vertical. 
+    Faça um programa que solicite o nome do usuário e imprima-o na vertical.
+"""
+
+if __name__ == '__main__':
+    nome = str(input(print("Informe um nome")))
+
+    for letra in nome:
+        print(letra)
+        
